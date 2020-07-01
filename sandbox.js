9c48c56b0b3f9fc3ac04bc8fba97020b84f8bc4b
patrick <patrick204nqh@gmail.com>
2020-07-01 22:17:11 +0700

    strict comparison

--- sandbox.js
+++ sandbox.js
@@ -1,30 +1,15 @@
-// booleans & comparisons
-// console.log(true, false, 'true', 'false');
-
-// method can return booleans
-let email = 'patrick204nqh@gmail.com';
-let names = ['mario', 'patrick', 'kevin'];
-
-// let result = email.includes('!');
-// let result = names.includes('patrick');
-
-// console.log(result);
-
-// comparison operators
 let age = 25;
 
+// loose comparison (different types can still be equal)
+
 // console.log(age == 25);
-// console.log(age == 30);
+// console.log(age == '25');
 // console.log(age != 25);
-// console.log(age > 20);
-// console.log(age < 20);
-// console.log(age <= 25);
-// console.log(age >= 25);
+// console.log(age != '25');
 
-let name = 'patrick';
+// strict comparison (different types cannot be equal)
 
-console.log(name == 'patrick');
-console.log(name == 'Patrick');
-console.log(name > 'robert');
-console.log(name > 'Patrick');
-console.log(name > 'Robert');
+console.log(age === 25);
+console.log(age === '25');
+console.log(age !== 25);
+console.log(age !== '25');
