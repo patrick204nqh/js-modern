673cec540c2d0ea8bcf6ac0e76775a8d0010c7f6
patrick <patrick204nqh@gmail.com>
2020-07-01 22:11:56 +0700

    boolean & comparison

--- sandbox.js
+++ sandbox.js
@@ -1,3 +1,30 @@
-let age = null;
+// booleans & comparisons
+// console.log(true, false, 'true', 'false');
 
-console.log(age, age + 3, `the age is ${age}`);
+// method can return booleans
+let email = 'patrick204nqh@gmail.com';
+let names = ['mario', 'patrick', 'kevin'];
+
+// let result = email.includes('!');
+// let result = names.includes('patrick');
+
+// console.log(result);
+
+// comparison operators
+let age = 25;
+
+// console.log(age == 25);
+// console.log(age == 30);
+// console.log(age != 25);
+// console.log(age > 20);
+// console.log(age < 20);
+// console.log(age <= 25);
+// console.log(age >= 25);
+
+let name = 'patrick';
+
+console.log(name == 'patrick');
+console.log(name == 'Patrick');
+console.log(name > 'robert');
+console.log(name > 'Patrick');
+console.log(name > 'Robert');
