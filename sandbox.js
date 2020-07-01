18004ea9b9b591c325363faa81a27135e55f4120
patrick <patrick204nqh@gmail.com>
2020-07-01 21:48:23 +0700

    null & undefined

--- sandbox.js
+++ sandbox.js
@@ -1,22 +1,3 @@
-let dev = ['patrick', 'kevin', 'robert', 'karl'];
+let age = null;
 
-// dev[1] = 'de bruyne';
-// console.log(dev[1]);
-
-// let ages = [20, 25, 30, 35];
-// console.log(ages[2]);
-
-// let random = ['patrick', 'robert', 30, 20];
-// console.log(random);
-
-// console.log(dev.length);
-
-// array methods
-
-// let result = dev.join('-');
-// let result = dev.indexOf('robert');
-// let result = dev.concat(['ken', 'hayate']);
-let result = dev.push('ken');
-result = dev.pop();
-
-console.log(result);
+console.log(age, age + 3, `the age is ${age}`);
