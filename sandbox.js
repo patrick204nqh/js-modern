351887379c319347fb8b21c8eabad179d85a93a0
patrick <patrick204nqh@gmail.com>
2020-07-01 20:07:38 +0700

    string

--- sandbox.js
+++ sandbox.js
@@ -1,1 +1,19 @@
 alert('hello, world');
+
+// string concatenation
+let firstName = 'Karl';
+let lastName = 'Patrick';
+
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+// getting characters
+console.log(fullName[2]);
+
+// string length
+console.log(fullName.length);
+
+// string methods
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result);
