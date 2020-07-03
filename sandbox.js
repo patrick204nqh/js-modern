a77a034734735f9fee86e3733e2ded216c203432
patrick <patrick204nqh@gmail.com>
2020-07-04 02:18:18 +0700

    else if statement

--- sandbox.js
+++ sandbox.js
@@ -1,18 +1,11 @@
-// if statements
-// const age = 25;
+// else if statement
 
-// if (age > 20) {
-//   console.log('you are over 20 years old');
-// }
+const password = 'pass';
 
-// const devs = ['patrick', 'robert', 'kevin', 'karl'];
-
-// if(devs.length > 3) {
-//   console.log("that's a lot of devs");
-// }
-
-const password = 'p@ss';
-
-if (password.length >= 8) {
+if (password.length >= 12) {
+  console.log('that password is might strong');
+} else if (password.length >= 8) {
   console.log('that password is long enough!');
+} else {
+  console.log('password is not long enough');
 }
