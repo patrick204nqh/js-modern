e1432ddd02d72bf123fcb1bdbceb92059dd35882
patrick <patrick204nqh@gmail.com>
2020-07-04 02:14:26 +0700

    if statement

--- sandbox.js
+++ sandbox.js
@@ -1,8 +1,18 @@
-// do while loops
+// if statements
+// const age = 25;
 
-let i = 3;
+// if (age > 20) {
+//   console.log('you are over 20 years old');
+// }
 
-do {
-  console.log('val of i is: ', i);
-  i++;
-} while (i < 5);
+// const devs = ['patrick', 'robert', 'kevin', 'karl'];
+
+// if(devs.length > 3) {
+//   console.log("that's a lot of devs");
+// }
+
+const password = 'p@ss';
+
+if (password.length >= 8) {
+  console.log('that password is long enough!');
+}
