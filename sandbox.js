91f5f0f77671144defccf063aafd3968316c9b59
patrick <patrick204nqh@gmail.com>
2020-07-04 11:51:33 +0700

    function declaration & function expression

--- sandbox.js
+++ sandbox.js
@@ -1,14 +1,17 @@
-// variables & block scope
-let age = 30;
+great();
+great();
+great();
 
-if (true) {
-  let age = 40;
-  let name = 'patrick';
-  console.log('inside 1st code block: ', age, name);
+speak();
+speak();
+speak();
 
-  if (true) {
-    console.log('inside 2nd code block: ', age);
-  }
+// function declaration
+function great() {
+  console.log('hello there');
 }
 
-console.log('outside code block: ', age, name);
+// function expression
+const speak = function () {
+  console.log('good day!');
+};
