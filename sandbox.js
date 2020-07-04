753eb68d54fa2f5e8fc79d37c81d2455c1cd3cab
patrick <patrick204nqh@gmail.com>
2020-07-04 23:53:02 +0700

    object literals

--- sandbox.js
+++ sandbox.js
@@ -1,21 +1,35 @@
 // object literals
 
+// const blogs = [
+//   { title: 'why mac & cheese rules', likes: 30 },
+//   { title: '10 things to make with marmite', likes: 50 },
+// ];
+
+// console.log(blogs);
+
 let user = {
   name: 'patrick',
   age: 20,
   email: 'patrick@dev.co.uk',
   location: 'bao loc',
-  blogs: ['why mac & cheese rules', '10 things to make with marmite'],
-  login: function () {
+  blogs: [
+    { title: 'why mac & cheese rules', likes: 30 },
+    { title: '10 things to make with marmite', likes: 50 },
+  ],
+  login() {
     console.log('the user logged in');
   },
-  logout: function () {
+  logout() {
     console.log('the user logged out');
+  },
+  logBlogs() {
+    // console.log(this.blogs);
+    console.log('this user has written the following blogs');
+    this.blogs.forEach((blog) => {
+      console.log(blog.title, blog.likes);
+    });
   },
 };
 
-user.login();
-user.logout();
-
-const name = 'robert';
-name.toUpperCase();
+user.logBlogs();
+console.log(this);
