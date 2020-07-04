ca60a673bf6e3e6f43fa5c28585ecc258ec03615
patrick <patrick204nqh@gmail.com>
2020-07-05 00:02:10 +0700

    Math object

--- sandbox.js
+++ sandbox.js
@@ -1,35 +1,19 @@
-// object literals
+// Math object
 
-// const blogs = [
-//   { title: 'why mac & cheese rules', likes: 30 },
-//   { title: '10 things to make with marmite', likes: 50 },
-// ];
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.E);
 
-// console.log(blogs);
+const area = 7.7;
 
-let user = {
-  name: 'patrick',
-  age: 20,
-  email: 'patrick@dev.co.uk',
-  location: 'bao loc',
-  blogs: [
-    { title: 'why mac & cheese rules', likes: 30 },
-    { title: '10 things to make with marmite', likes: 50 },
-  ],
-  login() {
-    console.log('the user logged in');
-  },
-  logout() {
-    console.log('the user logged out');
-  },
-  logBlogs() {
-    // console.log(this.blogs);
-    console.log('this user has written the following blogs');
-    this.blogs.forEach((blog) => {
-      console.log(blog.title, blog.likes);
-    });
-  },
-};
+console.log(Math.round(area));
+console.log(Math.floor(area));
+console.log(Math.ceil(area));
+console.log(Math.trunc(area));
 
-user.logBlogs();
-console.log(this);
+// random numbers
+
+const random = Math.random();
+
+console.log(random);
+console.log(Math.round(random * 100));
