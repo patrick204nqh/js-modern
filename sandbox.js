63658c5363e25aa2b62a5d281446592ea4ce3532
patrick <patrick204nqh@gmail.com>
2020-07-04 12:12:47 +0700

    regular function & arrow function

--- sandbox.js
+++ sandbox.js
@@ -1,14 +1,10 @@
-// returning values
-
-// const speak = function (name = 'patrick', time = 'night') {
-//   return `good ${time} ${name}`;
+// regular function
+// const calcArea = function (radius) {
+//   return 3.14 * radius ** 2;
 // };
 
-// console.log(speak());
+// arrow function
+const calcArea = (radius) => 3.14 * radius ** 2;
 
-const calcArea = function (radius) {
-  return 3.14 * radius ** 2;
-};
-
-let area = calcArea(5);
-console.log(area);
+const area = calcArea(5);
+console.log('area is: ', area);
