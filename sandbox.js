36421448a36c31709bdf5f14f34fc0b49f26a3fd
patrick <patrick204nqh@gmail.com>
2020-07-07 01:24:38 +0700

    element.textContent

--- sandbox.js
+++ sandbox.js
@@ -1,10 +1,13 @@
-const title = document.querySelector('h1');
+// const content = document.querySelector('p');
 
-// title.setAttribute('style', 'margin: 50px');
+// console.log(content.classList);
+// content.classList.add('error');
+// content.classList.remove('error');
+// content.classList.add('success');
 
-console.log(title.style);
-console.log(title.style.color);
+const paras = document.querySelectorAll('p');
 
-title.style.margin = '50px';
-title.style.color = 'crimson';
-title.style.fontSize = '60px';
+console.log(paras[0].innerText);
+console.log(paras[0].textContent);
+
+paras.forEach((p) => {});
