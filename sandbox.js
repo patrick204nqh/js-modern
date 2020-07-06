b97d641ddc6de5a36a26fab2b447a07ca57085b2
patrick <patrick204nqh@gmail.com>
2020-07-07 00:39:58 +0700

    getElementById & getElementsByClassName & getElementsTagName

--- sandbox.js
+++ sandbox.js
@@ -1,11 +1,13 @@
-// const para = document.querySelector('div.error');
+// get an element by ID
+// const title = document.getElementById('page-title');
+// console.log(title);
 
-// console.log(para);
+// get elements by their class name
+// const errors = document.getElementsByClassName('error');
+// console.log(errors);
+// console.log(errors[0]);
 
-const paras = document.querySelectorAll('p');
-const errors = document.querySelectorAll('.error');
-// paras.forEach((para) => {
-//   console.log(para);
-// });
-
-console.log(errors);
+// get elements by their tag name
+const paras = document.getElementsByTagName('p');
+console.log(paras);
+console.log(paras[1]);
